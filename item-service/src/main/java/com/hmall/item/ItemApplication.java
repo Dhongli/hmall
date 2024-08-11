9df285877f96f1ad9a0ec42c133317ada0715585
Dhongli <37902035+Dhongli@users.noreply.github.com>
2024-08-11 21:43:57 +0800

    day03-微服务01

---
+++ item-service/src/main/java/com/hmall/item/ItemApplication.java
@@ -0,0 +1,20 @@
+package com.hmall.item;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author daihongli
+ * @version 1.0
+ * @ClassName ItemApplication
+ * @Description: TODO
+ * @Date 2024-08-11 12:47
+ */
+@MapperScan("com.hmall.item.mapper")
+@SpringBootApplication
+public class ItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemApplication.class, args);
+    }
+}
